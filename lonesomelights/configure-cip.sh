b6845a275ddc89f0e6188b973149b20ef582804e
Maximilian Scholze <maximilian.scholze@fau.de>
2015-01-16 02:51:24 +0100

    Improve autotools SFML check

--- lonesomelights/configure-cip.sh
+++ lonesomelights/configure-cip.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 autoreconf --install
-./configure CXX="/local/clang-3.5/bin/clang++" CPPFLAGS="-I/local/libsfml-2.1/include" LDFLAGS="-L/local/libsfml-2.1/lib -Wl,--rpath -Wl,/local/libsfml-2.1/lib"
+./configure CXX="/local/clang-3.5/bin/clang++" CPPFLAGS="-I/usr/include -I/local/libsfml-2.1/include" LDFLAGS="-L/local/libsfml-2.1/lib -Wl,--rpath -Wl,/local/libsfml-2.1/lib" PKG_CONFIG_PATH="/local/libsfml-2.1/lib/pkgconfig"
+
+
