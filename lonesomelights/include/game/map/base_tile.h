02aa5b71ff3037e250038f33f3d01c51c1e2c359
Maximilian Scholze <maximilian.scholze@fau.de>
2015-01-27 23:04:01 +0100

    Fix last commit (did not compile)

--- lonesomelights/include/game/map/base_tile.h
+++ lonesomelights/include/game/map/base_tile.h
@@ -14,7 +14,7 @@ class Map;
 class Timer;
 class Player;
 
-class BaseTile : public Attackable, public Tile {
+class BaseTile : public Tile {
 public:
 
 	static BaseTile create(const Map& map, unsigned int x, unsigned int y, const Player& player);
